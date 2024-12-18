b404e21f50d4637fc3093d6869c838858905ec67
Abdullahtahirrr <abdullah.tahirr7@gmail.com>
2024-12-18 18:32:02 +0500

    data added

--- SourceCode/app.py
+++ SourceCode/app.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     query_filter = analyze_query(user_query)
     if(query_filter == "Valid"):
         # Use Basic RAG for retrieval
-        retriever_type = "MultiQuery Retriever"
+        retriever_type = "Basic Simliarity Search"
         # relevant_docs = get_relevant_docs_basic(user_query)
         
 
