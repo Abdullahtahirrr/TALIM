febfa739e81cd469eeeb1f1910b62ff8be14bc6b
Imaan <imaanibrar86@gmail.com>
2025-03-05 00:22:43 +0500

    Footer version 2

---
+++ frontend/src/components/SimpleFooter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/SimpleFooter.css";
+
+const SimpleFooter = () => {
+  return (
+    <footer className="simple-footer">
+      <a href="/privacy-policy">Privacy Policy</a>
+      <a href="/contact">Contact Us</a>
+      <a href="/faq">FAQ</a>
+    </footer>
+  );
+};
+
+export default SimpleFooter;
